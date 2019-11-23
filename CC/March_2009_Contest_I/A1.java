50f70fe858f0cad51d41c55669c79dc207123f84
10YearsLate <souvik.itconsultant@gmail.com>
2019-11-23 20:48:21 +0530

    Update A1.java

--- CC/March_2009_Contest_I/A1.java
+++ CC/March_2009_Contest_I/A1.java
@@ -1,38 +1,63 @@
 import java.util.*;
+
 import java.io.*;
-import java.lang.Systems.*;
+
+import java.lang.System.*;
+
+public class solutionA1{
 
 
-public class A1{
-
-    public static void main(String[] args){
-    
-      Scanner sc=new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-      
-      int t=sc.nextInt();
-      while(t-->0){
-          boolean flag=false;
-          int n=sc.nextInt();
-          int m=sc.nextInt();
-          
-          int[] a=new int[n];
-          for(int i=0;i<n;i++){
-            a[i]=sc.nextInt();
-          }
-          a=Arrays.sort(a);
-          int low=0;
-          int high=n-1;
-          while(low<high){
-          
-            if(a[low]==m||a[high]==m ||(a[low]+a[high])==m) flag=true;
-            if(a[low]+a[high]>m) {high--;continue;}
-            if(a[low]+a[high]<m) {m-=a[low];low++}
-          }
-          
-      
-      }
-    
-    }
+	public static void main(String[] args){
+		
+		long start = System.currentTimeMillis();
+	
+		remedy();
+		
+		long end = System.currentTimeMillis();
+		float sec = (end - start) / 1000F; 
+		System.out.println(sec + " seconds");
+	
+	}
+	
+	public static void remedy(){
+		
+		Scanner sc=new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+		
+		int T=sc.nextInt();
+		while(T-->0){
+	
+		int N=sc.nextInt();
+		int M=sc.nextInt();
+		int[] a=new int[N];
+		for(int i=0;i<N;i++) a[i]=sc.nextInt();
+		
+		int sum;
+		
+		boolean flag=false;
+		
+		for(int i=0;i<(1<<N);i++){
+			
+			sum=0;
+			for(int j=0;j<N;j++){
+				
+				 if((i&(1<<j))>0){
+					 
+					 sum+=a[j];
+					 //System.err.println(T+"     "+a[j]);
+					 
+				 }
+				 
+			}
+			
+			if(sum==M) {System.out.println("Yes");flag=true;break;}
+		
+		}
+		
+		if(!flag) System.out.println("No");
+		
+		}
+		
+	}
 
 
 }
