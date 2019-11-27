3b32733d5d9d49b83511ad30058a3453da42106c
10YearsLate <souvik.itconsultant@gmail.com>
2019-11-27 14:36:23 +0000

    A.java

--- CF/Edu77/A.java
+++ CF/Edu77/A.java
@@ -1,1 +1,35 @@
+import java.util.*;
 
+import javafx.scene.control.SplitPane.Divider;
+
+import java.io.*;
+import java.lang.*;
+
+public class A{
+
+    public static void main(String[] args){
+
+        long start = System.currentTimeMillis();
+	
+		remedy();
+		
+		long end = System.currentTimeMillis();
+		float sec = (end - start) / 1000F; 
+		//System.out.println(sec + " seconds");
+    }
+
+    private static void remedy() {
+
+        Scanner sc=new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        int n=sc.nextInt();
+        while(n-->0){
+            long c=sc.nextInt();
+            long sum=sc.nextInt();
+            long d=sum/c;
+            long r=sum%c;
+            long res= ((c-r)*d*d) + (r*(d+1)*(d+1));
+            System.out.println(res);
+        }
+
+    }
+}
